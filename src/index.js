da8eacd5737e5cb2b49591142210bc08eb0edeea
NithiNWeb4 <lpun490@gmail.com>
2024-06-21 07:26:52 +0530

    firsdt commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Register from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Register />
+  </React.StrictMode>
+);
+
