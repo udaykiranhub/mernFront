8d8283a6b7124ce271b6063b3678c4ccf914fe30
Uday Kiran  Pedda <143621936+udaykiranhub@users.noreply.github.com>
2024-06-21 07:19:28 +0530

    Update URL in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/register', {
+      const response = await fetch('https://mernback-yle9.onrender.com/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
